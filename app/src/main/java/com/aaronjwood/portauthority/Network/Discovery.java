b166cf6a5f2b5e039f679da73192abd820bd4424
Aaron Wood <aaronjwood@gmail.com>
2014-12-10 18:03:02 -0500

    Finished up the Discovery class. Now looks at and parses the ARP table after making sure the table is filled by making requests to hosts

--- app/src/main/java/com/aaronjwood/portauthority/Network/Discovery.java
+++ app/src/main/java/com/aaronjwood/portauthority/Network/Discovery.java
@@ -1,61 +1,85 @@
 package com.aaronjwood.portauthority.Network;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class Discovery {
+import com.aaronjwood.portauthority.R;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Comparator;
+
+public class Discovery extends AsyncTask<Void, Void, Void> {
+
+    private static final String TAG = "Discovery";
+
+    private Activity activity;
     private String ip;
-    private int port;
 
-    public Discovery(String ip, int port) {
+    public Discovery(Activity activity, String ip) {
+        this.activity = activity;
         this.ip = ip;
-        this.port = port;
     }
 
-    private int[] splitIp() {
-        String[] parts = this.ip.split(".");
-        int[] segments = new int[parts.length];
-        for (int i = 0; i < parts.length; i++) {
-            segments[i] = Integer.parseInt(parts[i]);
-        }
-
-        return segments;
-    }
-
-    private void isReachable() {
-        class Port implements Runnable {
-            int port;
-
-            Port(int port) {
-                this.port = port;
+    @Override
+    protected Void doInBackground(Void... params) {
+        String parts[] = this.ip.split("\\.");
+        for(int i = 1; i <= 255; i++) {
+            String newIp = parts[0] + "." + parts[1] + "." + parts[2] + "." + i;
+            InetAddress address;
+            try {
+                address = InetAddress.getByName(newIp);
+                address.isReachable(1);
             }
-
-            @Override
-            public void run() {
-                try {
-                    Socket s = new Socket();
-                    s.connect(
-                            new InetSocketAddress("192.168.1.149", this.port),
-                            1000);
-                    s.close();
-                    Log.d("TEST", String.valueOf(this.port));
-                }
-                catch (IOException e) {
-
-                }
-
+            catch(UnknownHostException e) {
+                Log.e(TAG, e.getMessage());
             }
-
+            catch(IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
         }
-        ;
-        for (int i = 1; i < 200; i++) {
-            Thread t = new Thread(new Port(i));
-            t.start();
-        }
+        return null;
     }
 
+    protected void onPostExecute(Void result) {
+        Toast.makeText(this.activity.getApplicationContext(), "Host discovery finished!", Toast.LENGTH_SHORT).show();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("/proc/net/arp"));
+            reader.readLine();
+            String line;
+
+            ListView hostList = (ListView) this.activity.findViewById(R.id.hostList);
+            ArrayAdapter<String> adapter = (ArrayAdapter<String>) hostList.getAdapter();
+
+            while((line = reader.readLine()) != null) {
+                String[] l = line.split("\\s+");
+                String flag = l[2];
+                String macAddress = l[3];
+                if(!flag.equals("0x0") && !macAddress.equals("00:00:00:00:00:00")) {
+                    adapter.add(l[0]);
+                }
+            }
+            adapter.sort(new Comparator<String>() {
+                @Override
+                public int compare(String lhs, String rhs) {
+                    return lhs.compareTo(rhs);
+                }
+            });
+            adapter.notifyDataSetChanged();
+        }
+        catch(FileNotFoundException e) {
+            Log.e(TAG, e.getMessage());
+        }
+        catch(IOException e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
 }
