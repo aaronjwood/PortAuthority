8211fe56c1f57806235a91f81e8c5e5b1256d321
Aaron Wood <aaronjwood@gmail.com>
2014-12-10 18:08:07 -0500

    Implemented the onSaveInstanceState method to save the list of hosts when the phone is rotated and such. Created the listener for when the host discovery button is clicked to start the discovery process. Cleaned up imports. Restore the state of the host list in the onCreate method

--- app/src/main/java/com/aaronjwood/portauthority/MainActivity.java
+++ app/src/main/java/com/aaronjwood/portauthority/MainActivity.java
@@ -5,30 +5,51 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.aaronjwood.portauthority.Network.Discovery;
 import com.aaronjwood.portauthority.Network.Wireless;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
-    private Wireless wifi;
     private final static int TIMER_INTERVAL = 1500;
+    private Wireless wifi;
+
+    private Button discoverHosts;
+    private ArrayList<String> hosts = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private ListView hostList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.wifi = new Wireless(this);
+        if(savedInstanceState != null) {
+            this.hosts = savedInstanceState.getStringArrayList("hosts");
+            this.adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, this.hosts);
+            this.hostList = (ListView) findViewById(R.id.hostList);
+            this.hostList.setAdapter(this.adapter);
+            this.adapter.notifyDataSetChanged();
+        }
 
-        this.wifi
-                .getExternalIpAddress((TextView) findViewById(R.id.externalIpAddress));
+        this.wifi = new Wireless(this);
+        this.wifi.getExternalIpAddress((TextView) findViewById(R.id.externalIpAddress));
+
+        final String internalIp = this.wifi.getInternalIpAddress();
 
         TextView macAddress = (TextView) findViewById(R.id.deviceMacAddress);
         macAddress.setText(this.wifi.getMacAddress());
 
         TextView ipAddress = (TextView) findViewById(R.id.internalIpAddress);
-        ipAddress.setText(this.wifi.getInternalIpAddress());
+        ipAddress.setText(internalIp);
 
         final Handler mHandler = new Handler();
         mHandler.postDelayed(new Runnable() {
@@ -41,6 +62,31 @@ public class MainActivity extends Activity {
                 mHandler.postDelayed(this, TIMER_INTERVAL);
             }
         }, 0);
+
+        this.discoverHosts = (Button) findViewById(R.id.discoverHosts);
+
+        discoverHosts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Finding hosts on your network...", Toast.LENGTH_SHORT).show();
+
+                hosts.clear();
+
+                adapter = new ArrayAdapter<>(v.getContext(), android.R.layout.simple_list_item_1, hosts);
+                hostList = (ListView) findViewById(R.id.hostList);
+                hostList.setAdapter(adapter);
+
+                Discovery discovery = new Discovery((Activity) v.getContext(), internalIp);
+                discovery.execute();
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedState) {
+        super.onSaveInstanceState(savedState);
+
+        savedState.putStringArrayList("hosts", this.hosts);
     }
 
     @Override
@@ -57,7 +103,7 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if(id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
