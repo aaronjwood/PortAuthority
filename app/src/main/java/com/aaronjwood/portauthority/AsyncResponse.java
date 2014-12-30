8b426369493dc1133e22e741a0e9df9e210f396c
Aaron Wood <aaronjwood@gmail.com>
2014-12-30 00:50:19 -0500

    Extended the interface by overloading the processFinish method

--- app/src/main/java/com/aaronjwood/portauthority/AsyncResponse.java
+++ app/src/main/java/com/aaronjwood/portauthority/AsyncResponse.java
@@ -1,5 +1,10 @@
 package com.aaronjwood.portauthority;
 
+import java.util.ArrayList;
+
 public interface AsyncResponse {
+
     void processFinish(String output);
+
+    void processFinish(ArrayList output);
 }
