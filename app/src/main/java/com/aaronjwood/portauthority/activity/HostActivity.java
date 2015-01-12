79c2ca6abfce855f9969e3d3b0364da0cf2eee89
Aaron Wood <aaronjwood@gmail.com>
2015-01-12 00:39:13 -0500

    Optimize imports

--- app/src/main/java/com/aaronjwood/portauthority/activity/HostActivity.java
+++ app/src/main/java/com/aaronjwood/portauthority/activity/HostActivity.java
@@ -22,7 +22,6 @@ import com.aaronjwood.portauthority.response.HostAsyncResponse;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
