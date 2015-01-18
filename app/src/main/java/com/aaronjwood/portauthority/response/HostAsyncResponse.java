4802b6c8b5416cbdee595b4b1cfa22c8482f4a8d
Aaron Wood <aaronjwood@gmail.com>
2015-01-18 01:06:17 -0500

    Docs for the response interfaces

--- app/src/main/java/com/aaronjwood/portauthority/response/HostAsyncResponse.java
+++ app/src/main/java/com/aaronjwood/portauthority/response/HostAsyncResponse.java
@@ -2,9 +2,24 @@ package com.aaronjwood.portauthority.response;
 
 public interface HostAsyncResponse {
 
+    /**
+     * Delegate to handle integer outputs
+     *
+     * @param output
+     */
     void processFinish(int output);
 
+    /**
+     * Delegate to handle boolean outputs
+     *
+     * @param output
+     */
     void processFinish(boolean output);
 
+    /**
+     * Delegate to handle string outputs
+     *
+     * @param output
+     */
     void processFinish(String output);
 }
--- app/src/main/java/com/aaronjwood/portauthority/response/MainAsyncResponse.java
+++ app/src/main/java/com/aaronjwood/portauthority/response/MainAsyncResponse.java
@@ -5,9 +5,24 @@ import java.util.Map;
 
 public interface MainAsyncResponse {
 
+    /**
+     * Delegate to handle array list outputs
+     *
+     * @param output
+     */
     void processFinish(ArrayList<Map<String, String>> output);
 
+    /**
+     * Delegate to handle integer outputs
+     *
+     * @param output
+     */
     void processFinish(int output);
 
+    /**
+     * Delegate to handle string outputs
+     *
+     * @param output
+     */
     void processFinish(String output);
 }
