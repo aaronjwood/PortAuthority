26e04b38d83a55753cb314de69d98fa3331e7d0b
Aaron Wood <aaronjwood@gmail.com>
2016-03-12 01:03:51 -0500

    Reduce code duplication by using the base abstract class

--- app/src/main/java/com/aaronjwood/portauthority/activity/LanHostActivity.java
+++ app/src/main/java/com/aaronjwood/portauthority/activity/LanHostActivity.java
@@ -5,7 +5,6 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,24 +21,14 @@ import com.aaronjwood.portauthority.response.HostAsyncResponse;
 import com.aaronjwood.portauthority.utils.Constants;
 import com.aaronjwood.portauthority.utils.UserPreference;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Map;
 
-
-public class LanHostActivity extends AppCompatActivity implements HostAsyncResponse {
+public class LanHostActivity extends HostActivity implements HostAsyncResponse {
     private Wireless wifi;
     private Host host = new Host();
     private String hostName;
     private String hostIp;
     private String hostMac;
     private ListView portList;
-    private ArrayAdapter<String> adapter;
-    private ArrayList<String> ports = new ArrayList<>();
     private ProgressDialog scanProgressDialog;
     private Dialog portRangeDialog;
     private int scanProgress;
@@ -276,128 +265,5 @@ public class LanHostActivity extends AppCompatActivity implements HostAsyncRespo
         if (output && this.portRangeDialog != null && this.portRangeDialog.isShowing()) {
             this.portRangeDialog.dismiss();
         }
-    }
-
-    /**
-     * Delegate to handle open ports
-     *
-     * @param output Contains the port number and associated banner (if any)
-     */
-    @Override
-    public void processFinish(Map<Integer, String> output) {
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new InputStreamReader(getAssets().open("ports.csv")));
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "Can't open port data file!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        String line;
-        int scannedPort = output.keySet().iterator().next();
-        String item = String.valueOf(scannedPort);
-
-        try {
-            while ((line = reader.readLine()) != null) {
-                String[] portInfo = line.split(",");
-                String name;
-                String port;
-
-                if (portInfo.length > 2) {
-                    name = portInfo[0];
-                    port = portInfo[1];
-                } else {
-                    name = "unknown";
-                    port = null;
-                }
-
-                if (name.isEmpty()) {
-                    name = "unknown";
-                }
-
-                int filePort;
-
-                //Watch out for inconsistent formatting of the CSV file we're reading!
-                try {
-                    filePort = Integer.parseInt(port);
-                } catch (NumberFormatException e) {
-                    continue;
-                }
-
-                if (scannedPort == filePort) {
-                    item = item + " - " + name;
-                    if (output.get(scannedPort) != null) {
-                        item += " (" + output.get(scannedPort) + ")";
-                    }
-
-                    if (scannedPort == 80 || scannedPort == 443 || scannedPort == 8080) {
-                        item += " \uD83C\uDF0E";
-                    }
-
-                    final String finalItem = item;
-
-                    runOnUiThread(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            ports.add(finalItem);
-
-                            Collections.sort(ports, new Comparator<String>() {
-
-                                @Override
-                                public int compare(String lhs, String rhs) {
-                                    int left = Integer.parseInt(lhs.substring(0, lhs.indexOf("-") - 1));
-                                    int right = Integer.parseInt(rhs.substring(0, rhs.indexOf("-") - 1));
-
-                                    return left - right;
-                                }
-                            });
-
-                            adapter.notifyDataSetChanged();
-                        }
-                    });
-
-                    reader.close();
-
-                    //Make sure to return so that we don't fall through and add the port again!
-                    return;
-                }
-            }
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "Error reading from port data file!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        //If a port couldn't be found in the port data file then make sure it's still caught and added to the list of open ports
-        item = item + " - unknown";
-        if (output.get(scannedPort) != null) {
-            item += " (" + output.get(scannedPort) + ")";
-        }
-
-        if (scannedPort == 80 || scannedPort == 443 || scannedPort == 8080) {
-            item += " \uD83C\uDF0E";
-        }
-
-        final String finalItem = item;
-
-        runOnUiThread(new Runnable() {
-
-            @Override
-            public void run() {
-                ports.add(finalItem);
-
-                Collections.sort(ports, new Comparator<String>() {
-
-                    @Override
-                    public int compare(String lhs, String rhs) {
-                        int left = Integer.parseInt(lhs.substring(0, lhs.indexOf("-") - 1));
-                        int right = Integer.parseInt(rhs.substring(0, rhs.indexOf("-") - 1));
-
-                        return left - right;
-                    }
-                });
-
-                adapter.notifyDataSetChanged();
-            }
-        });
     }
 }
