43919e9c0e97a8153812dbbf97adf7faa3a3c5a2
Aaron Wood <aaronjwood@gmail.com>
2016-04-01 00:02:52 -0400

    Sort the hosts on the UI thread so that we have proper visiblity between the list getting added to the adapter and the sorted array

--- app/src/main/java/com/aaronjwood/portauthority/activity/MainActivity.java
+++ app/src/main/java/com/aaronjwood/portauthority/activity/MainActivity.java
@@ -317,21 +317,21 @@ public class MainActivity extends Activity implements MainAsyncResponse {
      */
     @Override
     public void processFinish(final List<Map<String, String>> output) {
-        Collections.sort(output, new Comparator<Map<String, String>>() {
-
-            @Override
-            public int compare(Map<String, String> lhs, Map<String, String> rhs) {
-                int left = Integer.parseInt(lhs.get("Second Line").substring(lhs.get("Second Line").lastIndexOf(".") + 1, lhs.get("Second Line").indexOf("[") - 1));
-                int right = Integer.parseInt(rhs.get("Second Line").substring(rhs.get("Second Line").lastIndexOf(".") + 1, rhs.get("Second Line").indexOf("[") - 1));
-
-                return left - right;
-            }
-        });
-
         runOnUiThread(new Runnable() {
 
             @Override
             public void run() {
+                Collections.sort(output, new Comparator<Map<String, String>>() {
+
+                    @Override
+                    public int compare(Map<String, String> lhs, Map<String, String> rhs) {
+                        int left = Integer.parseInt(lhs.get("Second Line").substring(lhs.get("Second Line").lastIndexOf(".") + 1, lhs.get("Second Line").indexOf("[") - 1));
+                        int right = Integer.parseInt(rhs.get("Second Line").substring(rhs.get("Second Line").lastIndexOf(".") + 1, rhs.get("Second Line").indexOf("[") - 1));
+
+                        return left - right;
+                    }
+                });
+
                 SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(), output, R.layout.host_list_item, new String[]{"First Line", "Second Line"}, new int[]{android.R.id.text1, android.R.id.text2});
                 ListView hostList = (ListView) findViewById(R.id.hostList);
                 hostList.setAdapter(adapter);
