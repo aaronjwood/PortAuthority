8d2d1afbab7841d6032d7b4a1c71b95c4c2f5d7a
Aaron Wood <aaronjwood@gmail.com>
2016-05-24 21:53:42 -0400

    Start creating preferences

---
+++ app/src/main/java/com/aaronjwood/portauthority/activity/PreferencesActivity.java
@@ -0,0 +1,25 @@
+package com.aaronjwood.portauthority.activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+import com.aaronjwood.portauthority.R;
+
+public class PreferencesActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+}
