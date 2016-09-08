f6bc9d75e11b87c2d8892bd52b7c760180892f05
Aaron Wood <aaronjwood@gmail.com>
2016-09-08 12:06:01 -0400

    Docs explaining why we aren't using onPostExecute

--- app/src/main/java/com/aaronjwood/portauthority/async/ScanHostsAsyncTask.java
+++ app/src/main/java/com/aaronjwood/portauthority/async/ScanHostsAsyncTask.java
@@ -72,6 +72,8 @@ public class ScanHostsAsyncTask extends AsyncTask<String, Void, Void> {
     /**
      * Scans the ARP table and updates the list with hosts on the network
      * Resolves both DNS and NetBIOS
+     * Don't update the UI in onPostExecute since we want to do multiple UI updates here
+     * onPostExecute seems to perform all UI updates at once which would hinder what we're doing here
      * TODO: this method is gross, refactor it and break it up
      *
      * @param params
