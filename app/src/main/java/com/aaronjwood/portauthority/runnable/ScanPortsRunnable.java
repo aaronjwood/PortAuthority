c48a64f1192a78d12f4e9eb4ef9626b35f4633b5
Aaron Wood <aaronjwood@gmail.com>
2016-09-18 02:39:32 -0400

    Fix more potential memory leaks as well as a possible crash that can happen due to reference issues

--- app/src/main/java/com/aaronjwood/portauthority/runnable/ScanPortsRunnable.java
+++ app/src/main/java/com/aaronjwood/portauthority/runnable/ScanPortsRunnable.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.ref.WeakReference;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.HashMap;
@@ -14,7 +15,7 @@ public class ScanPortsRunnable implements Runnable {
     private String ip;
     private int startPort;
     private int stopPort;
-    private HostAsyncResponse delegate;
+    private final WeakReference<HostAsyncResponse> delegate;
 
     /**
      * Constructor to set the necessary data to perform a port scan
@@ -24,7 +25,7 @@ public class ScanPortsRunnable implements Runnable {
      * @param stopPort  Port to stop scanning at
      * @param delegate  Called when this chunk of ports has finished scanning
      */
-    public ScanPortsRunnable(String ip, int startPort, int stopPort, HostAsyncResponse delegate) {
+    public ScanPortsRunnable(String ip, int startPort, int stopPort, WeakReference<HostAsyncResponse> delegate) {
         this.ip = ip;
         this.startPort = startPort;
         this.stopPort = stopPort;
@@ -36,52 +37,56 @@ public class ScanPortsRunnable implements Runnable {
      */
     @Override
     public void run() {
-        for (int i = this.startPort; i <= this.stopPort; i++) {
-            this.delegate.processFinish(1);
+        HostAsyncResponse activity = this.delegate.get();
+        if (activity != null) {
+            for (int i = this.startPort; i <= this.stopPort; i++) {
 
-            HashMap<Integer, String> portData = new HashMap<>();
-            BufferedReader in;
-            String data = null;
-            Socket socket = new Socket();
-            socket.setPerformancePreferences(1, 0, 0);
+                activity.processFinish(1);
 
-            try {
-                socket.setTcpNoDelay(true);
-                socket.connect(new InetSocketAddress(this.ip, i), 4000);
+                HashMap<Integer, String> portData = new HashMap<>();
+                BufferedReader in;
+                String data = null;
+                Socket socket = new Socket();
+                socket.setPerformancePreferences(1, 0, 0);
 
-                //TODO: this is a bit messy, refactor and break it up
-                if (i == 22) {
-                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    data = in.readLine();
-                    in.close();
-                } else if (i == 80 || i == 443 || i == 8080) {
-                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                    out.println("GET / HTTP/1.1\r\nHost: " + this.ip + "\r\n");
-
-                    char[] buffer = new char[1024];
-                    in.read(buffer, 0, 1024);
-                    out.close();
-                    in.close();
-                    data = new String(buffer).toLowerCase();
-                    if (data.contains("apache") || data.contains("httpd")) {
-                        data = "Apache";
-                    } else if (data.contains("iis") || data.contains("microsoft")) {
-                        data = "IIS";
-                    } else if (data.contains("nginx")) {
-                        data = "Nginx";
-                    } else {
-                        data = null;
-                    }
-                }
-
-                portData.put(i, data);
-                this.delegate.processFinish(portData);
-            } catch (IOException ignored) {
-            } finally {
                 try {
-                    socket.close();
+                    socket.setTcpNoDelay(true);
+                    socket.connect(new InetSocketAddress(this.ip, i), 4000);
+
+                    //TODO: this is a bit messy, refactor and break it up
+                    if (i == 22) {
+                        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        data = in.readLine();
+                        in.close();
+                    } else if (i == 80 || i == 443 || i == 8080) {
+                        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                        out.println("GET / HTTP/1.1\r\nHost: " + this.ip + "\r\n");
+
+                        char[] buffer = new char[1024];
+                        in.read(buffer, 0, 1024);
+                        out.close();
+                        in.close();
+                        data = new String(buffer).toLowerCase();
+                        if (data.contains("apache") || data.contains("httpd")) {
+                            data = "Apache";
+                        } else if (data.contains("iis") || data.contains("microsoft")) {
+                            data = "IIS";
+                        } else if (data.contains("nginx")) {
+                            data = "Nginx";
+                        } else {
+                            data = null;
+                        }
+                    }
+
+                    portData.put(i, data);
+                    activity.processFinish(portData);
                 } catch (IOException ignored) {
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException ignored) {
+                    }
                 }
             }
         }
