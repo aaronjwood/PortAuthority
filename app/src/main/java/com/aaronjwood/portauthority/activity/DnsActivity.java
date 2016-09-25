e0ea23454c3dc4bcb190f94ee19c04e8871f031b
Aaron Wood <aaronjwood@gmail.com>
2016-09-25 13:53:30 -0400

    Save and fetch the last used domain name

--- app/src/main/java/com/aaronjwood/portauthority/activity/DnsActivity.java
+++ app/src/main/java/com/aaronjwood/portauthority/activity/DnsActivity.java
@@ -2,14 +2,30 @@ package com.aaronjwood.portauthority.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 import com.aaronjwood.portauthority.R;
+import com.aaronjwood.portauthority.utils.UserPreference;
 
 public final class DnsActivity extends AppCompatActivity {
+
+    private EditText domainName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dns);
+
+        this.domainName = (EditText) findViewById(R.id.domainName);
+        this.domainName.setText(UserPreference.getLastUsedDomainName(this));
+    }
+
+    /**
+     * Clean up
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        UserPreference.saveLastUsedDomainName(this, this.domainName.getText().toString());
     }
 }
