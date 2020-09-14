1bcc274923417bd69c9cf8b93a9319f072d95475
Aaron Wood <aaronjwood@gmail.com>
2020-09-14 03:31:02 -0700

    Display real progress when downloading databases

--- app/src/main/java/com/aaronjwood/portauthority/async/DownloadAsyncTask.java
+++ app/src/main/java/com/aaronjwood/portauthority/async/DownloadAsyncTask.java
@@ -14,13 +14,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.zip.GZIPInputStream;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public abstract class DownloadAsyncTask extends AsyncTask<Void, String, Void> {
+class DownloadProgress {
+    public String message;
+    public int progress;
 
+    @Override
+    public String toString() {
+        return message;
+    }
+}
+
+public abstract class DownloadAsyncTask extends AsyncTask<Void, DownloadProgress, Void> {
     private ProgressDialog dialog;
 
     protected Database db;
@@ -40,7 +48,10 @@ public abstract class DownloadAsyncTask extends AsyncTask<Void, String, Void> {
 
         Context ctx = (Context) activity;
         dialog = new ProgressDialog(ctx, R.style.DialogTheme);
+        dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         dialog.setMessage(ctx.getResources().getString(R.string.downloadingData));
+        dialog.setIndeterminate(false);
+        dialog.setMax(100);
         dialog.setCanceledOnTouchOutside(false);
         dialog.setOnCancelListener(dialogInterface -> {
             dialogInterface.cancel();
@@ -59,39 +70,51 @@ public abstract class DownloadAsyncTask extends AsyncTask<Void, String, Void> {
         BufferedReader in = null;
         HttpsURLConnection connection = null;
         db.beginTransaction();
+        DownloadProgress downProg = new DownloadProgress();
         try {
             URL url = new URL(service);
             connection = (HttpsURLConnection) url.openConnection();
             connection.setRequestProperty("Accept-Encoding", "gzip");
             connection.connect();
             if (connection.getResponseCode() != HttpsURLConnection.HTTP_OK) {
-                publishProgress(connection.getResponseCode() + " " + connection.getResponseMessage());
+                downProg.message = connection.getResponseCode() + " " + connection.getResponseMessage();
+                publishProgress(downProg);
 
                 return;
             }
 
+            // Get the content length ourselves since connection.getContentLengthLong() requires a minimum API of 24+.
+            // The same is true for connection.getHeaderFieldLong().
+            String contentLen = connection.getHeaderField("Content-Length");
+            long len = Long.parseLong(contentLen);
             in = new BufferedReader(new InputStreamReader(new GZIPInputStream(connection.getInputStream()), "UTF-8"));
             String line;
-
+            long total = 0;
             while ((line = in.readLine()) != null) {
                 if (isCancelled()) {
                     return;
                 }
 
+                total += line.length(); // Lean on the fact that we're working with UTF-8 here.
+                downProg.progress = (int) (total * 100 / len);
+                publishProgress(downProg);
                 String[] data = parser.parseLine(line);
                 if (data == null) {
                     continue;
                 }
 
                 if (parser.saveLine(db, data) == -1) {
-                    publishProgress("Failed to insert data into the database. Please run this operation again");
+                    downProg.message = "Failed to insert data into the database. Please run this operation again";
+                    publishProgress(downProg);
 
                     return;
                 }
             }
+
             db.setTransactionSuccessful();
         } catch (Exception e) {
-            publishProgress(e.toString());
+            downProg.message = e.toString();
+            publishProgress(downProg);
         } finally {
             db.endTransaction();
             try {
@@ -107,17 +130,16 @@ public abstract class DownloadAsyncTask extends AsyncTask<Void, String, Void> {
         }
     }
 
-    /**
-     * Handles errors.
-     *
-     * @param progress
-     */
     @Override
-    protected void onProgressUpdate(String... progress) {
+    protected void onProgressUpdate(DownloadProgress... progress) {
+        DownloadProgress currProg = progress[0];
         MainAsyncResponse activity = delegate.get();
-        if (activity != null) {
-            activity.processFinish(new Exception(progress[0]));
+        if (currProg.message != null && activity != null) {
+            activity.processFinish(new Exception(currProg.message));
+            return;
         }
+
+        dialog.setProgress(currProg.progress);
     }
 
     /**
